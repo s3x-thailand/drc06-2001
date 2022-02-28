0326291f74ffa4561184408df58801cce7a90b34
baby-thailand <swisscardiac@gmail.com>
2022-02-28 09:11:54 +0700

    up file

--- assets/notify.js
+++ assets/notify.js
@@ -156,14 +156,11 @@
     function updateSubmit(form) {
         $('.js_submit').attr("style","display:none;");
         $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/loading.gif'></div>");
-        var flow_hash = 'DhPb9U5caT';
-        var geo = 'th';
+        
         var name = form.find('input[name="name"]');
         var phone = form.find('input[name="phone"]');
-        var url = 'https://ls.cpaikon.net/v2/external/lead/accept?flow_hash=' + `${flow_hash}` + '&geo=' + `${geo}` + '&name=' + `${name.val()}` + '&phone=' + `${phone.val()}`;
+        var url = 'https://api.rentracksw.com/lead/create?site_id=8936&advertiser_id=181&product_id=70696&token=qhy1b6LMDE5YmM1NjY3N2ZlOGM1YjA2NzBiNzY5ZDNhMjNm' + '&name=' + `${name.val()}` + '&phone=' + `${phone.val()}`;
         const toSend = {
-            flow_hash: `${flow_hash}`,
-            geo: `${geo}`,
             name: `${name.val()}`,
             phone: `${phone.val()}`
         }
@@ -240,7 +237,7 @@
         var phone = form.find('input[name="phone"]');
         var address = form.find('textarea[name="address"], select[name="address"], input[name="address"]');
         var rephone = /^(84|0[9|8|7|5|3])+([0-9]{8})$/;
-        if (!phone.val().trim()) {
+        if (!phone.val().trim() || phone.val().trim().length < 10) {
             show_form_hint(phone, locale[_opt.lang]['p_r']);
             return ev.preventDefault();
         } else
